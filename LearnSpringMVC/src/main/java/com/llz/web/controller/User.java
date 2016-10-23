681e62dc1b4466ff94361646b64719d586a83d42
Beibei_owl <liliangzisweet@126.com>
2016-10-23 12:15:16 +0800

    update User.java

--- LearnSpringMVC/src/main/java/com/llz/web/controller/User.java
+++ LearnSpringMVC/src/main/java/com/llz/web/controller/User.java
@@ -15,7 +15,6 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	@Override
 	public String toString() {
 		return "User [username=" + username + ", password=" + password + "]";
 	}
